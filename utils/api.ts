398574c83c87e5d3117359c62d091b13a4f690b1
James Wang <jwang905@umd.edu>
2022-10-16 06:16:42 -0400

    sad :(

--- utils/api.ts
+++ utils/api.ts
@@ -1,3 +1,5 @@
+import fetch2 from 'node-fetch';
+
 const token = process.env["BEARER_TOKEN"];
 
 export async function search({track, artist, year}){
@@ -25,6 +27,8 @@ export async function search({track, artist, year}){
 };
 
 export async function addToPlaylist(playlist_id: string, uris: string){
+    console.log(uris);
+    
     const BASE_URL = `https://api.spotify.com/v1/playlists/${playlist_id}/tracks`;
     const params = new URLSearchParams({
         uris
@@ -33,15 +37,44 @@ export async function addToPlaylist(playlist_id: string, uris: string){
     
     console.log(API_URL);
     
-    const response = await fetch(API_URL,{
-        method: "POST",
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/json",
-            "Content-Length": "0",
-            "Authorization": "Bearer " + token,
-        }
+    // const response = await fetch(API_URL,{
+    //     method: "POST",
+    //     headers: {
+    //         "Accept": "application/json",
+    //         "Content-Type": "application/json",
+    //         "Content-Length": "0",
+    //         "Authorization": "Bearer " + token,
+    //     }
+    // });
+
+    const myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer " + token);
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Accept", "application/json");
+    myHeaders.append("Content-Length", "1000");
+    // fetch
+    console.log(myHeaders.get("Content-Length"));
+
+
+    const response = await fetch2(API_URL, {
+        method: 'POST',
+        headers: myHeaders,
     });
+    
+
+    // const response = await fetch2("https://api.spotify.com/v1/playlists/3Qow5iHDL82Ey0Og9LUTaS/tracks?uris=spotify%3Atrack%3A1eT2CjXwFXNx6oY5ydvzKU%2Cspotify%3Atrack%3A0wwPcA6wtMf6HUMpIRdeP7%2Cspotify%3Atrack%3A5xTtaWoae3wi06K5WfVUUH", {
+    //     "headers": {
+    //         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0",
+    //         "Accept": "*/*",
+    //         "Accept-Language": "en-US,en;q=0.5",
+    //         "Authorization": "Bearer BQBIS_mgovqhi__1w22o6k-aicYEVLqjvb27XDYngVaB2hItdBXHeWe-qr57dxPCHjk6Up1UigJL30nk-12q6PjS-TbXUOQ9wlbZ5cca4UZqS_Exx6d3m9Qz0Qyi0_QA-2GSmBb_LekqRtU3cKfbDkiaH66-8aq_Jj8D-QShlTNn0OMmGOj3cfXcr79Yc3XtkXVp0KSvLee3gSdYuEMGtrOYIB2oG8ZKpOqBhgVxSsNkJnzO5K07ROkVmLK0n8I_",
+    //         "content-length": "0",
+    //         "Content-Length": "0",
+    //     },
+    //     "referrer": "https://developer.spotify.com/",
+    //     "method": "POST",
+    // });
+
     if (response.ok) {
         return await response.json();   
     }
@@ -68,3 +101,54 @@ export async function addToPlaylist(playlist_id: string, uris: string){
 
 // xhr.send();
 // }
+
+export async function createPlaylist(name: string, user_id: string) {
+    const BASE_URL = `https://api.spotify.com/v1/users/${user_id}/playlists`;
+    const params = new URLSearchParams({
+        name
+    });
+    const API_URL = BASE_URL + '?' + params.toString();
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Content-Length": "0",
+            "Authorization": "Bearer " + token
+        }
+    });
+    if (response.ok) {
+        return await response.json();   
+    }
+    else {
+        return response.statusText;
+    }
+}
+
+// async function t() {
+//     console.log("hi");
+    
+//     const response = await fetch("https://api.spotify.com/v1/playlists/3Qow5iHDL82Ey0Og9LUTaS/tracks?uris=spotify%3Atrack%3A1eT2CjXwFXNx6oY5ydvzKU%2Cspotify%3Atrack%3A0wwPcA6wtMf6HUMpIRdeP7%2Cspotify%3Atrack%3A5xTtaWoae3wi06K5WfVUUH", {
+//         "headers": {
+//             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0",
+//             "Accept": "*/*",
+//             "Accept-Language": "en-US,en;q=0.5",
+//             "Authorization": "Bearer BQBIS_mgovqhi__1w22o6k-aicYEVLqjvb27XDYngVaB2hItdBXHeWe-qr57dxPCHjk6Up1UigJL30nk-12q6PjS-TbXUOQ9wlbZ5cca4UZqS_Exx6d3m9Qz0Qyi0_QA-2GSmBb_LekqRtU3cKfbDkiaH66-8aq_Jj8D-QShlTNn0OMmGOj3cfXcr79Yc3XtkXVp0KSvLee3gSdYuEMGtrOYIB2oG8ZKpOqBhgVxSsNkJnzO5K07ROkVmLK0n8I_",
+//             "Sec-Fetch-Dest": "empty",
+//             "Sec-Fetch-Mode": "cors",
+//             "Sec-Fetch-Site": "same-site",
+//             "Content-Length": "0"
+//         },
+//         "referrer": "https://developer.spotify.com/",
+//         "method": "POST",
+//     });
+//     console.log(response)
+//     if (response.ok) {
+//         return await response.json();   
+//     } else {
+//         console.log(response);
+//     }
+//     console.log(response);
+
+// }
+// t();
